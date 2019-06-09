fa77a13df2a89facdf6ee15547a814924d6c042a
Courtney Dyson <courtneydyson87@gmail.com>
2019-06-09 16:17:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
         return 'I can\'t hear you!'
       }
       else if (string === uppercase) {
-        return "YES INDEED!"
+        return "I love you, too."
       }
   
 }
