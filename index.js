487f7bec053c2264d03d5defad495683ff578290
Courtney Dyson <courtneydyson87@gmail.com>
2019-06-09 16:27:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 function shout(string) {
-  return string.toUpperCase();
+  return string.toUpperCase()
 }
 
 function whisper(string) {
-  return string.toLowerCase();
+  return string.toLowerCase()
 }
 
 function logShout(string) {
-  console.log('hello'.toUpperCase());
+  console.log('hello'.toUpperCase())
 }
 
 function logWhisper(string) {
-  console.log('HELLO'.toLowerCase());
+  console.log('HELLO'.toLowerCase())
 }
 
+function sayHiToGrandma(string) {
+  var uppercase = "HELLO"
+  var lowercase = "hello"
+  
+    if (string === uppercase) {
+      }else if (string === string.toUpperCase()) {
+        return "YES INDEED!"
+      }
+      else if (string === "I love you, Grandma.") {
+        return "I love you, too."
+      }
+  
+}
