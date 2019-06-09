a953f16811999da63704e9e4c9d24c3f5367a115
Courtney Dyson <courtneydyson87@gmail.com>
2019-06-09 16:20:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,12 +18,7 @@ function sayHiToGrandma(string) {
   var uppercase = "HELLO"
   var lowercase = "hello"
   
-    if (string === uppercase) {
-      }else if (string === string.toUpperCase()) {
-        return "YES INDEED!"
-      }
-      else if (string === "I love you, Grandma.") {
-        return "I love you, too."
-      }
-  
+  if(string === lowercase) {
+    if (string === string.toLowerCase)
+  }
 }
