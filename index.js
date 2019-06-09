c7fdbcbc42d4d212c5e92603350b5da8c01b64ad
Courtney Dyson <courtneydyson87@gmail.com>
2019-06-09 16:10:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
       }else if (string === string.toLowerCase()) {
         return 'I can\'t hear you!'
       }
-      else if (string === "I love you, Grandma.") {
+      else if (string === uppercase) {
         return "I love you, too."
       }
   
